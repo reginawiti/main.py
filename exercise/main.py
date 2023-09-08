1f0a2e13c485677e7c046c9f607771e9c9ee5f7d
reginawiti <143196467+reginawiti@users.noreply.github.com>
2023-09-08 18:06:39 +0300

    operations

---
+++ exercise/main.py
@@ -0,0 +1,51 @@
+# even numbers
+evenNumbers =(2,4,6)
+sumofevenNumbers =(2+4+6)
+print(sumofevenNumbers)
+
+#  numbers of students
+numbers =(10,20,30,40,50)
+sumofNumber =(10+20+30+40+50)
+Average =(sumofNumber)/5
+print(Average)
+
+-5 + 8 *6
+e = -5
+s =8
+t =6
+h = s * t
+print(e +h)
+
+(55 + 9)%9
+r = 55
+w = 9
+u = 9
+a =(r+ w)
+print(a%u)
+
+20 + -3 *5/8
+f =-3*(5/8)
+print(20 + f)
+
+5 + 15/3*5-8%3
+m = (5 +(15/3)*2)-8
+n = 3
+print(m%n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
